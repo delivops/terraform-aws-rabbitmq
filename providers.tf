73b7c31881bc1c7d4d78328071895e923e39cfc9
osnatashush <osnat@delivops.com>
2024-09-09 15:55:17 +0300

    set minimum version of aws/hashicorp to 3.0

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 0.13"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.31.0"
+      version = "~> 3.0"
     }
   }
 }
